a04276a55519d69bd1d0ce97406eee6b0a35a8bc
Sergey Smolin <smsergey@outlook.com>
2016-07-05 10:08:48 +0300

    Functional menu

--- components/Menu.js
+++ components/Menu.js
@@ -5,21 +5,20 @@ import navObjects from '../navObjects'
 
 // TODO: send menu items as props from somewhere else
 const Menu = ({ hoveredItem, onMouseOver, onMouseOut, ...other }) => {
-  let Dropdown = hoveredItem === parseInt(hoveredItem, 10) && navObjects[hoveredItem].component;
   return (
     <nav id="menu" className="navbar navbar-default navbar-static-top">
         <div id="navigation_container">
           <ul className="nav navbar-nav">
             {
               navObjects.map((obj, index) =>
-              <li onMouseOver={() => onMouseOver(index)} onMouseOut={() => onMouseOut()} className="nav_menu__item" key={index}>
-                  {
+              <li onMouseEnter={() => onMouseOver(index)} onMouseLeave={() => onMouseOut()} className="nav_menu__item" key={index}>
                       <NavLink to={'/' + obj.name}>{obj.value}</NavLink>
+                  {
+                      hoveredItem === index && <obj.component { ...other } className="menu_dropdown"/>
                   }
               </li>)
             }
           </ul>
-          { Dropdown && <Dropdown { ...other } className="menu_dropdown"/> }
         </div>
     </nav>
   )
