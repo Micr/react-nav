bf79c351f811a89292620bf32ec1eaf19a786c0a
Sergey Smolin <soees84@gmail.com>
2016-07-05 07:10:40 +0300

    Stange behaviour

--- components/Menu.js
+++ components/Menu.js
@@ -5,17 +5,22 @@ import navObjects from '../navObjects'
 import MenuDropdown from './MenuDropdown'
 
 // TODO: send menu items as props from somewhere else
-const Menu = ({ hoveredItem, onMouseOver, ...other }) => (
-  <nav id="menu" className="navbar navbar-default navbar-static-top">
-    <ul className="nav navbar-nav">
-      {
-        navObjects.map((obj, index) =>
-        <li onMouseOver={() => onMouseOver(index)} className="nav_menu__item" key={index}>
-            <NavLink to={'/' + obj.name}>{obj.value}</NavLink>{hoveredItem === index && <obj.component {...other} className="menu_dropdown"/>}
-        </li>)
-      }
-    </ul>
-  </nav>
-)
+const Menu = ({ hoveredItem, onMouseOver, ...other }) => {
+  let dropdown = hoveredItem === 0 || hoveredItem && navObjects[hoveredItem].component;
+  return (
+    <nav id="menu" className="navbar navbar-default navbar-static-top">
+      <ul className="nav navbar-nav">
+        {
+          navObjects.map((obj, index) =>
+          <li onMouseOver={() => onMouseOver(index)} className="nav_menu__item" key={index}>
+              {/*}<NavLink to={'/' + obj.name}>{obj.value}</NavLink>{hoveredItem === index && <obj.component {...other} className="menu_dropdown"/>}*/}
+              {<NavLink to={'/' + obj.name}>{obj.value}</NavLink>}
+          </li>)
+        }
+      </ul>
+      { dropdown && <dropdown /> }
+    </nav>
+  )
+}
 
 export default Menu;
