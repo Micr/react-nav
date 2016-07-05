93d2338670749a81a0df49d0be8e7452fd3f6348
Sergey Smolin <smsergey@outlook.com>
2016-07-05 09:25:57 +0300

    Improve nav dropdown

--- components/Menu.js
+++ components/Menu.js
@@ -6,19 +6,21 @@ import MenuDropdown from './MenuDropdown'
 
 // TODO: send menu items as props from somewhere else
 const Menu = ({ hoveredItem, onMouseOver, ...other }) => {
-  let dropdown = hoveredItem === 0 || hoveredItem && navObjects[hoveredItem].component;
+  let Dropdown = hoveredItem === parseInt(hoveredItem, 10) && navObjects[hoveredItem].component;
   return (
     <nav id="menu" className="navbar navbar-default navbar-static-top">
-      <ul className="nav navbar-nav">
-        {
-          navObjects.map((obj, index) =>
-          <li onMouseOver={() => onMouseOver(index)} className="nav_menu__item" key={index}>
-              {/*}<NavLink to={'/' + obj.name}>{obj.value}</NavLink>{hoveredItem === index && <obj.component {...other} className="menu_dropdown"/>}*/}
-              {<NavLink to={'/' + obj.name}>{obj.value}</NavLink>}
-          </li>)
-        }
-      </ul>
-      { dropdown && <dropdown /> }
+        <div id="navigation_container">
+          <ul className="nav navbar-nav">
+            {
+              navObjects.map((obj, index) =>
+              <li onMouseOver={() => onMouseOver(index)} className="nav_menu__item" key={index}>
+                  {<NavLink to={'/' + obj.name}>{obj.value}</NavLink>}
+              </li>)
+            }
+          </ul>
+          { Dropdown && <Dropdown { ...other } className="menu_dropdown"/> }
+
+        </div>
     </nav>
   )
 }
